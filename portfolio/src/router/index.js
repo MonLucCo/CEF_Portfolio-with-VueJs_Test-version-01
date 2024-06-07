8c1b30c5832d7bda0af5b41d0855e37f1ae76d27
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2024-06-07 19:22:14 +0200

    Etape4_Bug_Additif au comit Etape4

--- portfolio/src/router/index.js
+++ portfolio/src/router/index.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
-import NotFoundView from '@/views/NotFoundView.vue'
-import ProjectsNotFoundView from '@/views/ProjectsNotFoundView.vue'
+import ProjectUrlNotFoundView from '@/views/ProjectUrlNotFoundView.vue'
+import UrlNotFoundView from '@/views/UrlNotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,8 +11,8 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomeView },
     { path: '/about', name: 'about', component: AboutView },
     { path: '/projects', name: 'projects', component: ProjectsView },
-    { path: '/projects/:pathMatch(.*)*', name: 'project-not-found', component: ProjectsNotFoundView },
-    { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundView },
+    { path: '/:pathMatch(.*)*', name: 'url-not-found', component: UrlNotFoundView },
+    { path: '/projects/:pathMatch(.*)*', name: 'project-url-not-found', component: ProjectUrlNotFoundView },
   ]
 })
 
